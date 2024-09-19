b91103e2377e98f3e24c301b8a298de00c7c829d
vinicius <viniciusvwh1441@gmail.com>
2024-09-19 17:22:53 -0300

    export traces in http proto

--- src/infra/instrumentation.ts
+++ src/infra/instrumentation.ts
@@ -1,7 +1,7 @@
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
 import { Resource } from '@opentelemetry/resources'
 import { metrics, NodeSDK } from '@opentelemetry/sdk-node'
 import {
